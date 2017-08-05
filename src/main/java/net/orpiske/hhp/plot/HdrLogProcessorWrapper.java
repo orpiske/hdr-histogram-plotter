cbc494fa534e9f01ebe75281ae802cd26d815ace
Otavio Rodolfo Piske <opiske@redhat.com>
2017-08-05 09:36:28 +0200

    UPD: removed unused import

--- src/main/java/net/orpiske/hhp/plot/HdrLogProcessorWrapper.java
+++ src/main/java/net/orpiske/hhp/plot/HdrLogProcessorWrapper.java
@@ -3,7 +3,6 @@ package net.orpiske.hhp.plot;
 import org.HdrHistogram.HistogramLogProcessor;
 import org.apache.commons.io.FilenameUtils;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
