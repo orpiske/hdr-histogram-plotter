3a5cb28f39dfac730996f415aaaa035364fd9cc4
Otavio Rodolfo Piske <opiske@redhat.com>
2017-08-05 09:56:15 +0200

    UPD: replace OpenCSV with Apache Commons CSV

--- src/main/java/net/orpiske/hhp/plot/HdrReader.java
+++ src/main/java/net/orpiske/hhp/plot/HdrReader.java
@@ -1,31 +1,36 @@
 package net.orpiske.hhp.plot;
 
-import com.opencsv.CSVReader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.List;
 
 public class HdrReader {
 
     public HdrData read(String filename) throws FileNotFoundException, IOException {
-        CSVReader reader = new CSVReader(new FileReader(filename), ',');
+        Reader in = new FileReader(filename);
 
         HdrData ret = new HdrData();
         List<Double> value = ret.getValue();
         List<Double> percentile = ret.getPercentile();
 
+        Iterable<CSVRecord> records = CSVFormat.RFC4180
+                .withCommentMarker('#')
+                .withFirstRecordAsHeader()
+                .parse(in);
 
-        reader.readNext();
-        reader.readNext();
 
-        String [] nextLine;
-        while ((nextLine = reader.readNext()) != null) {
-            // nextLine[] is an array of values from the line
-            // System.out.println("0: " + nextLine[0] + "->" + nextLine[1]);
-            value.add(Double.parseDouble(nextLine[0]));
-            percentile.add(Double.parseDouble(nextLine[1]) * 100);
+        for (CSVRecord record : records) {
+            String valueStr = record.get(0);
+            String percentileStr = record.get(1);
+
+            value.add(Double.parseDouble(valueStr));
+            percentile.add(Double.parseDouble(percentileStr) * 100);
         }
 
         return ret;
