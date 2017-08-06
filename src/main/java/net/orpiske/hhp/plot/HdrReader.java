b2deef13805ee607595a4830ce75b1ae9d82e794
Otavio Rodolfo Piske <opiske@redhat.com>
2017-08-06 10:21:20 +0200

    REF: replaced stdout with logger

--- src/main/java/net/orpiske/hhp/plot/HdrReader.java
+++ src/main/java/net/orpiske/hhp/plot/HdrReader.java
@@ -3,6 +3,8 @@ package net.orpiske.hhp.plot;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -11,6 +13,7 @@ import java.io.Reader;
 import java.util.List;
 
 public class HdrReader {
+    private static final Logger logger = LoggerFactory.getLogger(HdrReader.class);
 
     public HdrData read(String filename) throws FileNotFoundException, IOException {
         Reader in = new FileReader(filename);
@@ -28,6 +31,9 @@ public class HdrReader {
         for (CSVRecord record : records) {
             String valueStr = record.get(0);
             String percentileStr = record.get(1);
+
+            logger.debug("Value: {}", valueStr);
+            logger.debug("Percentile: {}", percentileStr);
 
             value.add(Double.parseDouble(valueStr));
             percentile.add(Double.parseDouble(percentileStr) * 100);
