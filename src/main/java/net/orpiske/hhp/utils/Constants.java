cbf50ed63699a1f150f589cbb6f41f9f8f922d09
Otavio Rodolfo Piske <opiske@redhat.com>
2017-08-30 15:56:32 +0200

    FIX: append version to the help message

--- src/main/java/net/orpiske/hhp/utils/Constants.java
+++ src/main/java/net/orpiske/hhp/utils/Constants.java
@@ -24,7 +24,7 @@ public final class Constants {
 
 	public static final String VERSION = "1.0.0";
 
-	public static final String BIN_NAME = "hdr-histogram-plotter";
+	public static final String BIN_NAME = "hdr-histogram-plotter " + VERSION;
 
 
 	/**
